e79bb859506e3492b3816f09dbc4ffbc928e3547
racherb <racielhb@gmail.com>
2020-04-13 23:40:39 -0400

    Load unique tokens DataFrame
    loadUniqueTokensDataFrame

--- R/rtokens/man/tokens.R
+++ R/rtokens/man/tokens.R
@@ -1,12 +1,27 @@
+HOME_PATH <- getwd()
+DATA_PATH <- paste(HOME_PATH, "data", sep = "/")
+UTFILE_EN <- paste(DATA_PATH, "utokens_en.tab.gz", sep = "/")
+UTFILE_ES <- paste(DATA_PATH, "utokens_es.tab.gz", sep = "/")
+
+loadUniqueTokensDataFrame <- function(language = "en"){
+  if(language=="en") {tokens_df <- read.delim(UTFILE_EN)}
+  else if (language=="es") {tokens_df <- read.delim(UTFILE_ES)}
+  else {stop("There is no data for the defined language")}
+  return(tokens_df)
+}
+
+utokens_en <- loadUniqueTokensDataFrame("en")
+utokens_es <- loadUniqueTokensDataFrame("es")
+
 if(getRversion() >= "2.15.1")  utils::globalVariables(c("loadNamespace", "sparkHome"))
 
-# Setea variable de entorno con la ruta de Spark
+# Set environment variable with Spark's route
 setSparkHome <- function(sparkPath = "/usr/local/spark"){
   Sys.setenv(SPARK_HOME = sparkHome)
 }
 
-# Carga los datos como un DataFrame de Spark
-toSdf <- function(language = "en") {
+# Loads data as a Spark DataFrame
+toSparkDataFrame <- function(language = "en") {
   if (requireNamespace("SparkR", quietly = TRUE)) {
     stop("Package \"StarkR\" needed for this function to work. Please install it.",
          call. = FALSE)
